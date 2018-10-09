00ddbe8772b338e9ca1fcea2af8c9f99dafdc71e
tom <tom.guibard45@gmail.com>
2018-10-09 14:54:43 +0200

    Correction lié au message sur la Pull Request

--- app/routing.php
+++ app/routing.php
@@ -10,25 +10,15 @@
  */
 
 $routes = [
-    'Category' => [ // Controller
+    'Admin' => [ // Controller
         ['index', '/', 'GET'], // action, url, method
+    ],
+    'Category' => [ // Controller
+        ['index', '/category', 'GET'], // action, url, method
         ['add', '/category/add', ['GET', 'POST']], // action, url, method
         ['edit', '/category/edit/{id:\d+}', ['GET', 'POST']], // action, url, method
         ['show', '/category/{id:\d+}', 'GET'], // action, url, method
         ['delete', '/category/delete/{id:\d+}', 'GET'], // action, url, method
     ],
-    'Brands' => [ // Controller
-        ['index', '/brands', 'GET'], // action, url, method
-        ['add', '/brands/add', ['GET', 'POST']], // action, url, method
-        ['edit', '/brands/edit/{id:\d+}', ['GET', 'POST']], // action, url, method
-        ['show', '/brands/{id:\d+}', 'GET'], // action, url, method
-        ['delete', '/brands/delete/{id:\d+}', 'GET'], // action, url, method
-    ],
-    'Products' => [ // Controller
-        ['index', '/products', 'GET'], // action, url, method
-        ['add', '/products/add', ['GET', 'POST']], // action, url, method
-        ['edit', '/products/edit/{id:\d+}', ['GET', 'POST']], // action, url, method
-        ['show', '/products/{id:\d+}', 'GET'], // action, url, method
-        ['delete', '/products/delete/{id:\d+}', 'GET'], // action, url, method
-    ],
+
 ];

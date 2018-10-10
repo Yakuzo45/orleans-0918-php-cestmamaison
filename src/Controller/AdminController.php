73afab54b1f898079900e14daa6d0c9510b6d07a
tom <tom.guibard45@gmail.com>
2018-10-10 11:03:47 +0200

    Correction

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1,16 +1,10 @@
 <?php
 /**
  * Created by PhpStorm.
-<<<<<<< HEAD
- * User: root
- * Date: 11/10/17
- * Time: 16:07
- * PHP version 7
-=======
+
  * User: wilder7
  * Date: 09/10/18
  * Time: 14:45
->>>>>>> 00ddbe8772b338e9ca1fcea2af8c9f99dafdc71e
  */
 
 namespace Controller;

6d4c23e1709ee4546edc553b47bccc1c168e2ff4
Florence LG <flo.lg.gautier@gmail.com>
2018-10-18 15:12:52 +0200

    correction du ronting partie category, mettre que GET

--- app/routing.php
+++ app/routing.php
@@ -16,7 +16,7 @@ $routes = [
     ],
     'Category' => [ // Controller
         ['add', '/admin/category/add', ['GET', 'POST']],// action, url, method
-        ['index', '/admin/category/index', ['GET', 'POST']],// action, url, method
+        ['index', '/admin/category/index', ['GET']],// action, url, method
     ],
     'Item' => [ // Controller
         ['index', '/', 'GET'], // action, url, method

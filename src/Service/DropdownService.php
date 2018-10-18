1cb191367261b3225f31a19b4fcbd8f6fd3e1098
tom <tom.guibard45@gmail.com>
2018-10-18 10:25:18 +0200

    Amélioration des boutons dropdown

---
+++ src/Service/DropdownService.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder7
+ * Date: 18/10/18
+ * Time: 09:40
+ */
+
+namespace Service;
+
+use Controller\AbstractController;
+use Model\CategoryManager;
+use Model\BrandManager;
+
+class DropdownService extends AbstractController
+{
+    public function getCategories()
+    {
+        $categoryManager = new CategoryManager($this->getPdo());
+        $categories = $categoryManager->selectAll();
+
+        return $categories;
+    }
+
+    public function getBrands()
+    {
+        $brandManager = new BrandManager($this->getPdo());
+        $brand = $brandManager->selectAll();
+
+        return $brand;
+    }
+}

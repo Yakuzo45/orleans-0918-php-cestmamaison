d83b4d590975abb81878014eafe7387495233c4b
tom <tom.guibard45@gmail.com>
2018-10-19 11:46:09 +0200

    Corrections done

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -17,37 +17,39 @@ class CategoryController extends AbstractController
     public function add()
     {
         $errors = [];
-        $extension = ['png','jpg','jpeg'];
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             if (empty(trim($_POST['name']))) {
                 $errors['name'] = "La catégorie doit être renseignée";
             } elseif (strlen(trim($_POST['name'])) > 255) {
                 $errors['name'] = "La catégorie doit contenir moins de 255 caractères";
             } elseif (empty($_FILES['fichier']['name'])) {
-                $errors['name'] = 'Vous devez mettre une image';
+                $errors['name'] = 'L\'image doit être renseignée';
             } else {
-                if ($_FILES) {
-                    $length = filesize($_FILES['fichier']['tmp_name']);
-                    $ext = explode('.', $_FILES['fichier']['name']);
-                    if ($length > self::MAX_SIZE) {
-                        $errors['name'] = 'Votre fichier ne peut exceder 1Mo';
-                    } elseif (!in_array($ext[1], self::EXTENSION)) {
-                        $errors['name'] = 'Votre fichier peut uniquement posseder l\'extension ' . implode(' , ', self::EXTENSION);
-                    } else {
-                        $fileName = 'image' . uniqid() . '.' . $ext[1];
-                        $uploadDir = 'assets/images/CategoryImages/';
-                        $uploadFile = $uploadDir . basename($fileName);
-                        move_uploaded_file($_FILES['fichier']['tmp_name'], $uploadFile);
-                        $categoryManager = new CategoryManager($this->getPdo());
-                        $category = new Category;
-                        $category->setName(trim(($_POST['name'])));
-                        $category->setImage($fileName);
-                        $id = $categoryManager->insert($category);
-                        header('Location:/admin');
-                        exit();
 
-                    }
+                $length = filesize($_FILES['fichier']['tmp_name']);
+                $ext = pathinfo($_FILES['fichier']['name'], PATHINFO_EXTENSION);
+                if ($length > self::MAX_SIZE) {
+                    $errors['name'] = 'Votre fichier ne peut exceder 1Mo';
+                } elseif (!in_array($ext, self::EXTENSION)) {
+                    $errors['name'] = 'Votre fichier peut uniquement posseder l\'extension ' . implode(' , ', self::EXTENSION);
                 }
+            }
+
+            if (empty($errors)) {
+                $fileName = 'image' . uniqid() . '.' . $ext[1];
+                $uploadDir = 'assets/images/CategoryImages/';
+                $uploadFile = $uploadDir . basename($fileName);
+                move_uploaded_file($_FILES['fichier']['tmp_name'], $uploadFile);
+
+                $categoryManager = new CategoryManager($this->getPdo());
+                $category = new Category;
+                $category->setName(trim(($_POST['name'])));
+                $category->setImage($fileName);
+                $id = $categoryManager->insert($category);
+
+                header('Location:/admin');
+                exit();
+
             }
         }
         return $this->twig->render('Admin/Category/add.html.twig', ['error' => $errors]);

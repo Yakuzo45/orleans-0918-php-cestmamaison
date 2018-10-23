616e1785bfb2cefead07d4fae6efee9b40f220b8
Florence LG <flo.lg.gautier@gmail.com>
2018-10-23 22:38:10 +0200

    insertion de la public function insert product

--- src/Model/ProductManager.php
+++ src/Model/ProductManager.php
@@ -27,4 +27,15 @@ class ProductManager extends AbstractManager
      * @param Product $product
      * @return int
      */
+    public function insert(product $product):int
+    {
+        $statement = $this->pdo->prepare("INSERT INTO $this->table (`name`,`image`) VALUES (:name, :image)");
+        $statement->bindValue('name', $product->getName(), \PDO::PARAM_STR);
+        $statement->bindValue('image', $product->getImage(), \PDO::PARAM_STR);
+
+
+        if ($statement->execute()) {
+            return $this->pdo->lastInsertId();
+        }
+    }
 }

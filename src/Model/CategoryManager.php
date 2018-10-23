ad88777a1ca144c3b878f1fcd81a4bfe4e20d90c
tom <tom.guibard45@gmail.com>
2018-10-23 12:05:27 +0200

    Modification du PR

--- src/Model/CategoryManager.php
+++ src/Model/CategoryManager.php
@@ -27,9 +27,9 @@ class CategoryManager extends AbstractManager
     public function insert(Category $category): int
     {
         // prepared request
-        $statement = $this->pdo->prepare("INSERT INTO $this->table (`name`,`image`) VALUES (:name, :image)");
+        $statement = $this->pdo->prepare("INSERT INTO $this->table (`name`,`picture`) VALUES (:name, :picture)");
         $statement->bindValue('name', $category->getName(), \PDO::PARAM_STR);
-        $statement->bindValue('image', $category->getImage(), \PDO::PARAM_STR);
+        $statement->bindValue('picture', $category->getPicture(), \PDO::PARAM_STR);
 
 
         if ($statement->execute()) {

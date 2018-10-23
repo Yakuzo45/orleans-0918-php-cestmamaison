72e0b59310e0dd4c9b4757dfd52ab1ee9141b6ed
Florence LG <flo.lg.gautier@gmail.com>
2018-10-23 22:51:08 +0200

    rajout des parametres description et price dans la function isert

--- src/Model/ProductManager.php
+++ src/Model/ProductManager.php
@@ -29,9 +29,11 @@ class ProductManager extends AbstractManager
      */
     public function insert(product $product):int
     {
-        $statement = $this->pdo->prepare("INSERT INTO $this->table (`name`,`image`) VALUES (:name, :image)");
+        $statement = $this->pdo->prepare("INSERT INTO $this->table (`name`,`picture`) VALUES (:name, :picture)");
         $statement->bindValue('name', $product->getName(), \PDO::PARAM_STR);
-        $statement->bindValue('image', $product->getImage(), \PDO::PARAM_STR);
+        $statement->bindValue('picture', $product->getPicture(), \PDO::PARAM_STR);
+        $statement->bindValue('description',$product->getDescription(),\PDO::PARAM_STR);
+        $statement->bindValue('price',$product->getPrice(),\PDO::PARAM_STR);
 
 
         if ($statement->execute()) {

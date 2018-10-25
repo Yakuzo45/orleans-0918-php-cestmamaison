f47e3df7efee65e6c4479b6f77e8e579321ec568
Florence LG <flo.lg.gautier@gmail.com>
2018-10-25 16:15:54 +0200

    ajout des methodes category pour le ProductController

--- src/Model/Product.php
+++ src/Model/Product.php
@@ -15,6 +15,8 @@ class Product
     private $description;
     private $price;
     private $picture;
+    private $category_id;
+    private $brand_id;
 
 
     /**
@@ -33,6 +35,7 @@ class Product
     public function setId(int $id): Product
     {
         $this->id = $id;
+        return $this;
     }
 
 
@@ -52,6 +55,7 @@ class Product
     public function setName(string $name): Product
     {
         $this->name = $name;
+        return $this;
     }
 
 
@@ -71,6 +75,7 @@ class Product
     public function setDescription(string $description): Product
     {
         $this->description = $description;
+        return $this;
     }
 
 
@@ -90,6 +95,7 @@ class Product
     public function setPrice(float $price): Product
     {
         $this->price = $price;
+        return $this;
     }
 
 
@@ -109,5 +115,47 @@ class Product
     public function setPicture(string $picture): Product
     {
         $this->picture = $picture;
+        return $this;
     }
+
+    /**
+     * @return mixed
+     */
+    public function getBrandId()
+    {
+        return $this->brand_id;
+    }
+
+    /**
+     * @param mixed $brand_id
+     */
+    public function setBrandId($brand_id)
+    {
+        $this->brand_id = $brand_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCategoryId()
+    {
+        return $this->category_id;
+    }
+
+    /**
+     * @param mixed $category_id
+     */
+    public function setCategoryId($category_id)
+    {
+        $this->category_id = $category_id;
+    }
+
+    public function setCa(string $trim)
+    {
+    }
+
+    public function setCate(string $trim)
+    {
+    }
+
 }

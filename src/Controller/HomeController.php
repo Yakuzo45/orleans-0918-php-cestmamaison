7baa8e6f9f87a36e71bc053b74ba237e8684cfe0
Florence LG <flo.lg.gautier@gmail.com>
2018-10-31 13:39:05 +0100

    correction de la fonction contact pour les vérif côté serveur

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -46,48 +46,41 @@ class HomeController extends AbstractController
         ]);
     }
 
+    /**
+     * @return array|string
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
     public function contact()
     {
         return $this->twig->render('Visitor/Contact/index.html.twig');
-    }
 
-    private function checkErrors()
-    {
         $errorsForm=[];
-        if (empty($_post['firstName'])) {
-            $errorsForm['firstName'] = "Indiquer votre Prénom";
-        } elseif (!preg_match("/^[a-zA-Z0-9]+$/", $_POST['firstName'])) {
-            $errorsForm['firstName'] = "Votre prénom ne doit pas contenir des caractères spéciaux";
-        }
-        if (empty($_post['lastName'])) {
-            $errorsForm['lastName'] = "Indiquer votre Nom";
-        } elseif (!preg_match("/^[a-zA-Z0-9]+$/", $_POST['lastName'])) {
-            $errorsForm['lastName'] = "Votre nom ne doit pas contenir des caractères spéciaux";
-        }
-        if (empty($_post['msg'])) {
-            $errorsForm['msg'] = "Indiquer votre Message";
-        } elseif (!preg_match("/^[a-zA-Z0-9]+$/", $_POST['msg'])) {
-            $errorsForm['lastName'] = "Votre message ne doit pas contenir des caractères spéciaux";
-        }
-        if (empty($_post['email'])) {
-            $errorsForm['email'] = "Indiquer votre Mail";
-        } elseif (!preg_match("/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{,}$/", $_POST['lastName'])) {
-            $errorsForm['email'] = "Votre mail  doit être correctement en format email";
-        }
-        return $errorsForm;
-    }
-
-    /**
-     * @return array
-     */
-    public function validContactForm() :array
-    {
-        $errorsForm = [];
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             foreach ($_POST as $key => $value){
                 $cleanPost[$key]=trim($value);
             }
-            $errorsForm = $this->checkErrors();
+
+            if (empty($_post['firstName'])) {
+                $errorsForm['firstName'] = "Indiquer votre Prénom";
+            } elseif (!preg_match("/^[a-zA-Z0-9]+$/", $_POST['firstName'])) {
+                $errorsForm['firstName'] = "Votre prénom ne doit pas contenir des caractères spéciaux";
+            }
+            if (empty($_post['lastName'])) {
+                $errorsForm['lastName'] = "Indiquer votre Nom";
+            } elseif (!preg_match("/^[a-zA-Z0-9]+$/", $_POST['lastName'])) {
+                $errorsForm['lastName'] = "Votre nom ne doit pas contenir des caractères spéciaux";
+            }
+            if (empty($_post['msg'])) {
+                $errorsForm['msg'] = "Indiquer votre Message";
+            }
+
+            if (empty($_post['email'])) {
+                $errorsForm['email'] = "Indiquer votre Mail";
+            } elseif (!preg_match("/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{,}$/", $_POST['lastName'])) {
+                $errorsForm['email'] = "Votre mail  doit être correctement en format email";
+            }
             header('location:/visitor/contact/index.html.twig');
             exit();
         }
